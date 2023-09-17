1323245135fffaad8c2684fed5ff746a7444e384
sainath-chilakalapudi <007nathsai@gmail.com>
2023-09-17 23:59:37 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 // Wait for the document to be ready
 $(document).ready(function () {
+  $(window).scroll(function(){
+    // sticky navbar o scroll script
+    if(this.scrollY > 20){
+      $('.header').addClass("sticky");
+    }else{
+      $('.header').removeClass("sticky")
+    }
+
+    // scroll-up button show/hide script
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+  });
+
+  $('.header .menu li a').click(function(){
+    // applying again smooth scrll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $('.menu-btn').click(function(){
+    $('.header .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
     // jQuery code to execute once the document is ready
 
     // Example: When the button is clicked, change its text
